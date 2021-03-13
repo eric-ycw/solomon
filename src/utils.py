4925b6909a0cd2c8496a8d346d62a184201f83ec
Eric Yip <ericycw512@gmail.com>
2021-03-13 18:21:13 +0800

    Rewrite utils with OOP

--- src/utils.py
+++ src/utils.py
@@ -1,29 +1,52 @@
 import sys
 import numpy as np
 
-### Cost functions ###
-def mse(y, y_hat):
-    # Mean squared error
-    return np.square(y - y_hat).mean()
+class Activation:
+    def val(self, x): pass
+    def deriv(self, x): pass
 
-### Feature scaling ###
-def minmax_norm(features, min=-1, max=1):
-    # Minmax normalization
-    return min + (max - min) * (features - features.min(0)) / features.ptp(0)
+class Sigmoid(Activation):
+    def val(self, x):
+        return 1 / (1 + np.exp(-x))
 
-def mean_norm(features):
-    # Mean normalization
-    return (features - features.mean(0)) / features.std(0)
+    def deriv(x):
+        return self.val(x) / (1 - self.val(x))
 
-### Optimization ###
-def gradient_descent(gradient, features, step):
-    # Simplest form of gradient descent
-    return features - gradient * step
+class Loss:
+    def loss(self, y, y_hat): pass
+    def gradient(self, y, y_hat): pass
+
+class MeanSquaredError(Loss):
+    def loss(self, y, y_hat):
+        return np.square(y - y_hat).mean()
+
+    def gradient(self, y, y_hat):
+        return -(y - y_hat)
+
+class Normalization:
+    def normalize(self, features): pass
+
+class Rescale(Normalization):
+    def normalize(self, features, min=-1, max=1):
+        return min + (max - min) * (features - features.min(0)) / features.ptp(0)
+
+class MeanNormalization(Normalization):
+    def normalize(self, features):
+        return (features - features.mean(0)) / features.std(0)
+
+class Optimization:
+    def optimize(self, features): pass
+
+class BatchGradientDescent(Optimization):
+    def optimize(self, X, errors, weights, learning_rate):
+        delta = X.T.dot(errors) / X.shape[0]
+
+        return weights - delta * learning_rate
 
 ### Miscellaneous ###
-def show_progress(epoch, max_epoch, cost, bar_size=40):
+def show_progress(epoch, max_epoch, loss, bar_size=40):
     prog = (epoch + 1) / max_epoch
-    cost_str = 'Cost: ' + str(cost)
+    loss_str = 'Loss: ' + str(loss)
     sys.stdout.write('\r')
-    sys.stdout.write(f"[{'=' * int(bar_size * prog):{bar_size}s}] {int(100 * prog)}%  {cost_str}")
+    sys.stdout.write(f"[{'=' * int(bar_size * prog):{bar_size}s}] {int(100 * prog)}%  {loss_str}")
     sys.stdout.flush()
